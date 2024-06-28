f11d7f9df89b2acffdca734d2070533dd8b3aa70
ouafcode <ouafaesaim@gmail.com>
2024-06-28 12:37:35 +0000

    task14

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const str = [];
+  for (const report of reportWithIterator){
+    str.push(report);
+  }
+  return str.join(' | ');
+}
